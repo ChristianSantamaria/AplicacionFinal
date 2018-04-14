8b2c6ad3dbcef8dda2b0f24560ce4415a57fcb4c
ChristianSantamaria <christianpopi@hotmail.com>
2018-04-14 21:02:29 +0200

    Base de Datos Producto

---
+++ app/src/main/java/com/afinal/aplicacionfinal/DataBase.kt
@@ -0,0 +1,97 @@
+package com.afinal.aplicacionfinal
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import android.widget.Toast
+
+val DATABASE_NAME ="DataBase"
+val TABLE_NAME="Products"
+val COL_ID = "ID"
+val COL_NAME_PRODUCT = "Name"
+val COL_PRICE = "Price"
+val COL_DURATION = "Duration"
+
+
+class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,null,1){
+
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        val createTable = "CREATE TABLE " + TABLE_NAME +" (" + COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," + COL_NAME_PRODUCT + " VARCHAR(256)," + COL_PRICE +" INTEGER," + COL_DURATION + " INTEGER);";
+        System.out.println(createTable)
+        db?.execSQL(createTable)
+
+        System.out.println("Paso")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?,oldVersion: Int,newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun insertProduct(product : Product){
+        val db = this.writableDatabase
+        var cv = ContentValues()
+
+        cv.put(COL_NAME_PRODUCT, product.name)
+        cv.put(COL_PRICE, product.price)
+        cv.put(COL_DURATION, product.duration)
+
+
+
+        var result = db.insert(TABLE_NAME, null, cv)
+        if(result == -1.toLong())
+            Toast.makeText(context,"Error",Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(context,"Insertado Correctamente",Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun readData() : MutableList<Product>{
+        var list : MutableList<Product> = ArrayList()
+
+        val db = this.readableDatabase
+        val query = "Select * from " + TABLE_NAME
+        val result = db.rawQuery(query,null)
+        if(result.moveToFirst()){
+            do {
+                var user = Product()
+                user.id = result.getString(result.getColumnIndex(COL_ID)).toInt()
+                user.name = result.getString(result.getColumnIndex(COL_NAME_PRODUCT))
+                user.price = result.getString(result.getColumnIndex(COL_PRICE)).toInt()
+                user.duration = result.getString(result.getColumnIndex(COL_DURATION)).toInt()
+                list.add(user)
+            }while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return list
+    }
+
+    fun deleteData(){
+        val db = this.writableDatabase
+        db.delete(TABLE_NAME,null,null)
+        db.close()
+    }
+
+    fun updateData() {
+        val db = this.writableDatabase
+        val query = "Select * from " + TABLE_NAME
+        val result = db.rawQuery(query,null)
+        if(result.moveToFirst()){
+            do {
+                var cv = ContentValues()
+                cv.put(COL_PRICE,(result.getInt(result.getColumnIndex(COL_PRICE))+1))
+                db.update(TABLE_NAME,cv,COL_ID + "=? AND " + COL_NAME_PRODUCT + "=?",
+                        arrayOf(result.getString(result.getColumnIndex(COL_ID)),
+                                result.getString(result.getColumnIndex(COL_NAME_PRODUCT))))
+            }while (result.moveToNext())
+        }
+
+        result.close()
+        db.close()
+    }
+
+
+}
--- app/src/main/java/com/afinal/aplicacionfinal/MainActivity.kt
+++ app/src/main/java/com/afinal/aplicacionfinal/MainActivity.kt
@@ -10,6 +10,9 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -55,6 +58,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
@@ -62,7 +67,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 // Handle the camera action
             }
             R.id.nav_gallery -> {
-
+                val intent = Intent(this,  MainProductActivity::class.java)
+                startActivity(intent)
             }
             R.id.nav_slideshow -> {
 

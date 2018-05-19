228c81ecc31bc5712874b65e4762ee047c328b78
ChristianSantamaria <christianpopi@hotmail.com>
2018-05-19 17:23:21 +0200

    BD

--- app/src/main/java/com/afinal/aplicacionfinal/NuevoDiaRutina.kt
+++ app/src/main/java/com/afinal/aplicacionfinal/NuevoDiaRutina.kt
@@ -1,19 +1,28 @@
 package com.afinal.aplicacionfinal
 
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_nuevo_dia_rutina.*
 
 class NuevoDiaRutina : AppCompatActivity() {
     var db = DataBaseHandler(this)
-    var comidas = ArrayList<Product>()
+    var comidas = ArrayList<ArrayList<Product>>()
 
+    var aux = ArrayList<Product>()
+    var aux2 = ArrayList<Product>()
+    var aux3 = ArrayList<Product>()
+    var aux4 = ArrayList<Product>()
+    var aux5 = ArrayList<Product>()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_dia_rutina)
 
-        var p1 = Product("Zanahorias", 2f, 4, "lacteo", R.drawable.carrots)
+        var p1 = Product(1,"Zanahorias", 2f, 4, "lacteo", R.drawable.carrots)
 
         agregarDesayuno.setOnClickListener {
             agregarProducto(p1,"Desayuno")
@@ -32,20 +41,28 @@ class NuevoDiaRutina : AppCompatActivity() {
         }
 
         guardarRutina.setOnClickListener {
-            var resultData = Intent()
-            resultData.putExtra("rutinaSeleccionada", comidas)
-            setResult(RESULT_OK, resultData)
-            finish()
+//            var resultData = Intent()
+//            resultData.putExtra("rutinaSeleccionada", comidas)
+//            setResult(RESULT_OK, resultData)
+//            finish()
+            db.insertarRutinaModelo(1,1,1,1,1,1)
+            db.insertarRutina(1,1,1,1,1,1)
+            comidas.add(aux)
+            comidas.add(aux2)
+            comidas.add(aux3)
+            comidas.add(aux4)
+            comidas.add(aux5)
         }
     }
     fun agregarProducto(producto: Product, tipo: String) {
-        //db.insertarCualquierComida(producto,tipo)
+        db.insertarCualquierComida(1,producto.id,tipo)
+
         when(tipo) {
-            "Desayuno"    -> comidas.add(producto)
-            "Almuerzo"    -> comidas.add(producto)
-            "Comida"      -> comidas.add(producto)
-            "Merienda"    -> comidas.add(producto)
-            "Cena"        -> comidas.add(producto)
+            "Desayuno"    -> aux.add(producto)
+            "Almuerzo"    -> aux2.add(producto)
+            "Comida"      -> aux3.add(producto)
+            "Merienda"    -> aux4.add(producto)
+            "Cena"        -> aux5.add(producto)
         }
     }
 

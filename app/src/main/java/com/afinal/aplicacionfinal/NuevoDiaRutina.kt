c5bd22d64c659e070b9004a3c47618f2ac1d429d
kevinlemavilanova <kevinlemavilanova@gmail.com>
2018-05-20 11:52:57 +0200

    rutina mejorada

--- app/src/main/java/com/afinal/aplicacionfinal/NuevoDiaRutina.kt
+++ app/src/main/java/com/afinal/aplicacionfinal/NuevoDiaRutina.kt
@@ -1,5 +1,6 @@
 package com.afinal.aplicacionfinal
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,36 +9,54 @@ import kotlinx.android.synthetic.main.activity_nuevo_dia_rutina.*
 class NuevoDiaRutina : AppCompatActivity() {
     var db = DataBaseHandler(this)
     var comidas = ArrayList<Product>()
+    var request_code = 1
+    var p1 = Product()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_dia_rutina)
 
-        var p1 = Product("Zanahorias", 2f, 4, "lacteo", R.drawable.carrots)
 
         agregarDesayuno.setOnClickListener {
+            val intent = Intent(this,  MainProductActivity::class.java)
+            startActivityForResult(intent, request_code)
             agregarProducto(p1,"Desayuno")
         }
         agregarAlmuerzo.setOnClickListener {
+            val intent = Intent(this,  MainProductActivity::class.java)
+            startActivityForResult(intent, request_code)
             agregarProducto(p1,"Almuerzo")
         }
         agregarComida.setOnClickListener {
+            val intent = Intent(this,  MainProductActivity::class.java)
+            startActivityForResult(intent, request_code)
             agregarProducto(p1,"Comida")
         }
         agregarMerienda.setOnClickListener {
+            val intent = Intent(this,  MainProductActivity::class.java)
+            startActivityForResult(intent, request_code)
             agregarProducto(p1,"Merienda")
         }
         agregarCena.setOnClickListener {
+            val intent = Intent(this,  MainProductActivity::class.java)
+            startActivityForResult(intent, request_code)
             agregarProducto(p1,"Cena")
         }
 
         guardarRutina.setOnClickListener {
-            var resultData = Intent()
+            var resultData = Intent(this, Rutina::class.java)
             resultData.putExtra("rutinaSeleccionada", comidas)
-            setResult(RESULT_OK, resultData)
+            setResult(Activity.RESULT_OK, resultData)
             finish()
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent){
+        if ((requestCode == request_code) && (resultCode == RESULT_OK)){
+            p1 = data.extras.get("producto") as Product
+        }
+    }
+
     fun agregarProducto(producto: Product, tipo: String) {
         //db.insertarCualquierComida(producto,tipo)
         when(tipo) {
